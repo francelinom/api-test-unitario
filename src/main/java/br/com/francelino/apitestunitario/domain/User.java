5e30d0baad562291f2f08f0f2b4c80e45ad04b80
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-04-04 14:07:01 -0300

    Classe usuario

---
+++ src/main/java/br/com/francelino/apitestunitario/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.francelino.apitestunitario.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
