ac7bf0a33bb3a5cb011e79a02abdca15343b359f
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-04-04 15:34:44 -0300

    interface user repository

---
+++ src/main/java/br/com/francelino/apitestunitario/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.francelino.apitestunitario.repositories;
+
+import br.com.francelino.apitestunitario.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
