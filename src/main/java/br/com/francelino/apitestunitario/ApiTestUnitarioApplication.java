37db3e61451f36a8fe55e3db6dbb599ddea33a36
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-04-04 12:16:48 -0300

    first commit

---
+++ src/main/java/br/com/francelino/apitestunitario/ApiTestUnitarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.francelino.apitestunitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestUnitarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestUnitarioApplication.class, args);
+    }
+
+}
