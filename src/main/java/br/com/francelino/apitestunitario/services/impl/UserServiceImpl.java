307fdff79af56dc7df5d232f971f4cd383b74269
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-04-10 21:25:46 -0300

    feat tratamento de exceção para objeto não encontrado

--- src/main/java/br/com/francelino/apitestunitario/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/francelino/apitestunitario/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.francelino.apitestunitario.services.impl;
 import br.com.francelino.apitestunitario.domain.User;
 import br.com.francelino.apitestunitario.repositories.UserRepository;
 import br.com.francelino.apitestunitario.services.UserService;
+import br.com.francelino.apitestunitario.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
