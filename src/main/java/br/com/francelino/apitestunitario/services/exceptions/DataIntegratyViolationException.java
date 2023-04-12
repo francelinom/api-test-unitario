64d3ef2e7bacacbd5fe991c0137c34e953ae7656
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-04-12 20:41:50 -0300

    feat criando excecao para email já cadastrado

---
+++ src/main/java/br/com/francelino/apitestunitario/services/exceptions/DataIntegratyViolationException.java
@@ -0,0 +1,8 @@
+package br.com.francelino.apitestunitario.services.exceptions;
+
+public class DataIntegratyViolationException extends RuntimeException{
+
+    public DataIntegratyViolationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/br/com/francelino/apitestunitario/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/francelino/apitestunitario/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.francelino.apitestunitario.domain.User;
 import br.com.francelino.apitestunitario.domain.dto.UserDTO;
 import br.com.francelino.apitestunitario.repositories.UserRepository;
 import br.com.francelino.apitestunitario.services.UserService;
+import br.com.francelino.apitestunitario.services.exceptions.DataIntegratyViolationException;
 import br.com.francelino.apitestunitario.services.exceptions.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,8 +34,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(UserDTO obj) {
+        findByEmail(obj);
         return userRepository.save(mapper.map(obj, User.class));
     }
 
+    private void findByEmail(UserDTO obj) {
+        Optional<User> user = userRepository.findByEmail(obj.getEmail());
+        if (user.isPresent()) {
+            throw new DataIntegratyViolationException("Email já cadastrado no sistema");
+        }
+    }
 
 }
